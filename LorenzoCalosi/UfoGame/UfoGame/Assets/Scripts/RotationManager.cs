bc92c1d66d955e08a12d581073d5acebe1e2a30d
Lorenzo Calosi <lorenzo.calosi1@gmail.com>
2017-02-15 20:13:48 +0100

    Esercizio 1

---
+++ LorenzoCalosi/UfoGame/UfoGame/Assets/Scripts/RotationManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationManager : MonoBehaviour {
+
+	//Update is called every frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+	}
+}
